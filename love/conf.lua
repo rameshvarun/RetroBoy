ba4786f99b5bed07c071a213a02c1efe0e019180
Varun Ramesh <varunramesh@live.com>
2015-10-08 19:56:07 -0700

    retroboy.filesystem should work on Unix machines

--- love/conf.lua
+++ love/conf.lua
@@ -1,6 +1,7 @@
 function love.conf(t)
+  t.identity = "retroboy"
+
   t.window.title = "RetroBoy"
-  
   t.window.width = 640
   t.window.height = 576
 end
--- love/filesystem.lua
+++ love/filesystem.lua
@@ -1,4 +1,5 @@
 local FileSystem = class("FileSystem")
+local prettyprint = require "prettyprint"
 
 function FileSystem:initialize(path)
   self.prefix = path .. '/'
@@ -12,7 +13,27 @@ end
 function FileSystem:read(name)
   local f = io.open(self.prefix .. name, "rb")
   local data = f:read("*a")
+  f:close()
+  
   return data
 end
+
+function FileSystem:enumerate(path)
+	-- TODO: Make this more portable.
+	local files = {}
+	for file in io.popen('ls ' .. self.prefix .. path):lines() do
+		table.insert(files, file)
+	end
+	return files
+end
+
+function FileSystem:isFile(path)
+	return os.execute('test -f ' .. self.prefix .. path) == 0
+end
+
+function FileSystem:isDir(path)
+	return os.execute('test -d ' .. self.prefix .. path) == 0
+end
+
 
 return FileSystem
